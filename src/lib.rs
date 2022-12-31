45894c1989e3002dd001e6a013aaabca8939d4e1
Ubugeeei <ubuge1122@gamil.com>
2022-12-31 15:36:14 +0900

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+#[macro_export]
+macro_rules! map {
+    ($([$k:expr]: $v:expr),* $(,)?) => {
+        {
+            let mut temp_vec = std::collections::HashMap::new();
+            $(
+                temp_vec.insert($k, $v);
+            )*
+            temp_vec
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_map() {
+        {
+            let map = map! {
+                [1]: "one",
+                [2]: "two",
+                [3]: "three",
+            };
+            assert_eq!(map.get(&1), Some(&"one"));
+            assert_eq!(map.get(&2), Some(&"two"));
+            assert_eq!(map.get(&3), Some(&"three"));
+        }
+
+        {
+            let map = map! {
+                ["first"]: 1,
+                ["second"]: 2,
+                ["third"]: 3,
+            };
+            assert_eq!(map.get(&"first"), Some(&1));
+            assert_eq!(map.get(&"second"), Some(&2));
+            assert_eq!(map.get(&"third"), Some(&3));
+        }
+    }
+}
